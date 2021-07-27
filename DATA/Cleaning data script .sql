50741076cb9a8d0efd302bb077047feeaeeda350
Drilima7 <86249757+Drilima7@users.noreply.github.com>
2021-07-27 14:51:24 -0700

    Add files via upload

--- DATA/Cleaning data script .sql
+++ DATA/Cleaning data script .sql
@@ -264,27 +264,41 @@ from dim_address;
 delete from dim_address
 where address_id in (32,33); 
 
--- case when
+-- case when 
+-- Creating a new column for the student's nationality. Using the majority of my date. Case when country is US we will then have them as americans in our new nationality colunm.
+-- For the next one when country is GB then we will have them as British in our new Nationality column.
 
-  select *,
-        CASE WHEN address_id = 1 THEN 'US'
-        ELSE 'OTHER' END AS ADDRESS_extra
+        
+		select *,
+        CASE WHEN country = 'US'  THEN 'AMERICANS'
+        ELSE 'OTHER' END AS Nationality
         from dim_address
         
         ;
         
+        select *,
+        CASE WHEN country = 'GB' THEN 'British'
+        ELSE 'OTHER' END AS Nationality
+        from dim_address
+        ;
+        
+        
+        
+	
         -- COALESCE 
+        -- COALESCE N/A for the null states presented in my database.
         
           SELECT 
-            category,
-            age,
-            coalesce(gender, 'FEMALE') AS gender
-            FROM dim_student
+            country,
+            coalesce(state, 'N/A') as state
+            FROM dim_address
             ;
         
+		
         
         -- NULLIFF 
-        
+        -- From my student table I am selecting category, age and the gender of my students. I am using the query nullif for student's gender. 
+        -- Nullif the gender is male. All the results will return as NULL for the male students. 
         SELECT 
             category,
             age,
@@ -292,34 +306,39 @@ where address_id in (32,33);
             FROM dim_student
             ;
         
-  -- LEAST/GREATEST 
+ -- GREATEST 
   
-         SELECT 
-              state,
-              gender,
-              age,
-              before_environment,
+  --  I am showing below the students with homework stress level 3 or greater before the pandemic and we can see if their stress level increased  or decreased with the pandemic. 
+  -- We will visualize the student id, how was their environment(virtual or physical school) before pandemic and now. 
+  -- With that information we can see how the change of the enviromment affetected their stress level.
+  ;
+  
+SELECT 
+	student_id,
+	before_environment,
+    greatest(3, before_homeworkStress ) as before_homeworkStress,
+    before_environment,
+    now_homeworkStress
+	FROM fact_table;
+              
+   
+   -- I am showing below only the students who are facing a high level of stress with the pandemic. So I am asking for student id, the type of enviromnent (virtual or physical school).
+   -- Homework stress at least 5 (what is the higer level on my dataset).
+		SELECT 
+              student_id,
               now_environment,
-              greatest(2, before_homeworkStress) as before_homeworkStress,
-              now_homeworkStress
-              FROM temp_table
+              least(5, now_homeworkStress ) as now_homeworkStress
+              FROM fact_table
               
               ;
               
-              SELECT 
-              state,
-              gender,
-              age,
-              before_environment,
-              now_environment,
-              least(5, now_homeworkStress) as now_homeworkStress
-              FROM temp_table
-              
-              ;
+ALTER table fact_table rename column now_hhomeworkStress to now_homeworkStress ;              
 
  ALTER table temp_table rename column now_hhomeworkStress to now_homeworkStress ;
  
+ 
+-- I am using the distinct state to know all states from my address table.
  Select 
-       distinct country
-       from dim_address ;
+       distinct state
+       from dim_address;
  
